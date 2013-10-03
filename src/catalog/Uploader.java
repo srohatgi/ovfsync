e45a1eeeffc75e06b8c1474fbad3fb1fa9f88e0b
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2013-10-02 18:24:20 -0700

    refactored even more

--- src/catalog/Uploader.java
+++ src/catalog/Uploader.java
@@ -1,6 +1,5 @@
 package catalog;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -28,88 +27,61 @@ import com.vmware.vcloud.sdk.Vdc;
 import com.vmware.vcloud.sdk.constants.Version;
 import com.vmware.vcloud.sdk.samples.FakeSSLSocketFactory;
 
-/**
- * 1. UPLOAD VAPPTEMPLATE :: Uploads the selfcontained ovf xml which contains a
- * vapp and a blank vm(1 vcpu and 512 mb) with no disks/nics.
- * 
- * 2. ADD VAPP TEMPLATE TO CATALOG :: Attaches the uploaded vapptemplate to a
- * catalog.
- * 
- * 3. COMPOSE NEW VAPP :: Composes a vapp (with 'n' Blank VM's) using the newly
- * uploaded vapptemplate.
- * 
- * @author Ecosystem Engineering
- */
+import edu.princeton.cs.introcs.StdIn;
 
+/**
+ * Upload image templates to an organization catalog
+ */
 public class Uploader {
 
   private static VcloudClient vcloudClient;
   private static Vdc vdc;
-  private static VappTemplate vappTemplate;
-  private static Vapp vapp;
-
-  private static String imageDir = "src/main/resources";
-  private static String nameOvfRhel = "rhel6.3_64.ovf";
-  private static String nameOvfBlankVm = "blankVM.ovf";
-  private static String vmdkfileName = "rhel6.3_64-disk1.vmdk";
 
   /**
    * Uploading the ovf package(vapp with 1 blank vm) as a vapp template.
    * 
-   * @param vAppTemplateName
-   * @param vAppTemplateDesc
+   * @param name
+   * @param desc
+   * @param vmdkFilename TODO
    * @param ovfStream
    * @param vmdkStream
-   *          TODO
    * @return {@link VappTemplate}
    * @throws VCloudException
    * @throws IOException
    * @throws InterruptedException
    */
-  public static VappTemplate uploadVappTemplate(String vAppTemplateName,
-      String vAppTemplateDesc, InputStream ovfStream, InputStream vmdkStream)
+  public static VappTemplate uploadVappTemplate(String name,
+      String desc, String vmdkFilename, InputStream ovfStream, InputStream vmdkStream)
       throws VCloudException, IOException, InterruptedException {
 
     // Creating an vapptemplate with the provided name and description
-    UploadVAppTemplateParamsType vappTemplParams = new UploadVAppTemplateParamsType();
-    vappTemplParams.setDescription(vAppTemplateDesc);
-    vappTemplParams.setName(vAppTemplateName);
-    VappTemplate vappTemplate = vdc.createVappTemplate(vappTemplParams);
-    /*
-     * String ovfFileLocation = imageDir + "/" + nameOvfRhel; File ovfFile = new
-     * File(ovfFileLocation); FileInputStream ovfFileInputStream = new
-     * FileInputStream(ovfFile); vappTemplate.uploadOVFFile(ovfFileInputStream,
-     * ovfFile.length());
-     */
+    UploadVAppTemplateParamsType params = new UploadVAppTemplateParamsType();
+    params.setDescription(desc);
+    params.setName(name);
+    VappTemplate vtmpl = vdc.createVappTemplate(params);
 
-    vappTemplate.uploadOVFFile(ovfStream, ovfStream.available());
+    vtmpl.uploadOVFFile(ovfStream, ovfStream.available());
 
-    vappTemplate = VappTemplate.getVappTemplateByReference(vcloudClient,
-        vappTemplate.getReference());
-    // waiting until the ovf descriptor uplaoded flag is true.
-    while (!vappTemplate.getResource().isOvfDescriptorUploaded()) {
-      vappTemplate = VappTemplate.getVappTemplateByReference(vcloudClient,
-          vappTemplate.getReference());
-    }
+    // TODO: what's the point of below code??
+    vtmpl = VappTemplate.getVappTemplateByReference(vcloudClient,
+        vtmpl.getReference());
 
     // waiting until the vapptemplate gets resolved.
-    while (vappTemplate.getResource().getStatus() != 8
-        && !vappTemplate.getResource().isOvfDescriptorUploaded()) {
+    while (vtmpl.getResource().getStatus() != 8
+        && !vtmpl.getResource().isOvfDescriptorUploaded()) {
       System.out.println("Verifying if ovfFile is uploaded..");
       Thread.sleep(500);
     }
 
-    ovfStream.close();
+    vtmpl.uploadFile(vmdkFilename, vmdkStream, vmdkStream.available());
 
-    /*
-     * String vmdkFileLocation = imageDir + "/" + vmdkfileName; File vmdkFile =
-     * new File(vmdkFileLocation); FileInputStream vmdkFileInputStream = new
-     * FileInputStream(vmdkFile); vappTemplate.uploadFile(vmdkfileName,
-     * vmdkFileInputStream, vmdkFile.length());
-     */
+    // waiting until the vapptemplate gets resolved.
+    while (vtmpl.getResource().getStatus() != 8) {
+      System.out.println("Verifying if vmdkFile is uploaded..");
+      Thread.sleep(500);
+    }
 
-    vappTemplate.uploadFile(vmdkfileName, vmdkStream, vmdkStream.available());
-    return vappTemplate;
+    return vtmpl;
 
   }
 
@@ -121,15 +93,16 @@ public class Uploader {
    * @throws VCloudException
    */
   private static void addVappTemplateToCatalog(ReferenceType catalogRef,
-      String vAppTemplateName) throws VCloudException {
+      VappTemplate tmpl, String name, String desc) throws VCloudException {
+
     if (catalogRef == null) {
-      System.out.println("Catalog not found");
-      System.exit(0);
+      throw new RuntimeException("catalogRef is null");
     }
+
     CatalogItemType catalogItemType = new CatalogItemType();
-    catalogItemType.setName(vAppTemplateName);
-    catalogItemType.setDescription(vAppTemplateName);
-    catalogItemType.setEntity(vappTemplate.getReference());
+    catalogItemType.setName(name);
+    catalogItemType.setDescription(desc);
+    catalogItemType.setEntity(tmpl.getReference());
     Catalog catalog = Catalog.getCatalogByReference(vcloudClient, catalogRef);
     catalog.addCatalogItem(catalogItemType);
   }
@@ -146,7 +119,7 @@ public class Uploader {
    * @throws VCloudException
    * @throws TimeoutException
    */
-  private static void createVapp(String vAppName, String vmName,
+  private static void createVapp(VappTemplate vtmpl, String vAppName, String vmName,
       Integer noOfBlankVms) throws VCloudException, TimeoutException {
     ComposeVAppParamsType composeVAppParamsType = new ComposeVAppParamsType();
     composeVAppParamsType.setName(vAppName);
@@ -158,21 +131,21 @@ public class Uploader {
     for (int i = 0; i < noOfBlankVms; i++) {
       ReferenceType vappTemplateRef = new ReferenceType();
       vappTemplateRef.setName(vmName + "-" + i);
-      vappTemplateRef.setHref(vappTemplate.getChildren().get(0).getReference()
+      vappTemplateRef.setHref(vtmpl.getChildren().get(0).getReference()
           .getHref());
       SourcedCompositionItemParamType vappTemplateItem = new SourcedCompositionItemParamType();
       vappTemplateItem.setSource(vappTemplateRef);
       composeVAppParamsType.getSourcedItem().add(vappTemplateItem);
     }
 
-    vapp = vdc.composeVapp(composeVAppParamsType);
+    Vapp vapp = vdc.composeVapp(composeVAppParamsType);
     vapp.getTasks().get(0).waitForTask(0);
     vapp = Vapp.getVappByReference(vcloudClient, vapp.getReference());
 
   }
 
   /**
-   * Creating Vapp with 'n' blank vm's sample.
+   * main
    * 
    * @param args
    * @throws VCloudException
@@ -195,8 +168,8 @@ public class Uploader {
       System.err.println("USAGE");
       System.err.println("-----");
       System.err
-          .println("vcloudUrl username password orgName vdcName existingCatalogName");
-      System.exit(0);
+          .println("Uploader <username> <password> <orgName> <vdcName> <existingCatalogName>");
+      System.exit(1);
     }
 
     String vCloudUrl = args[0];
@@ -207,63 +180,93 @@ public class Uploader {
     String vdcName = args[4];
     String catalogName = args[5];
 
-    // default args
+    // testing args
     vCloudUrl = "https://uklo1.symphonycloud.savvis.com";
     orgName = "Symphony1";
     vdcName = "SS_DEMO_6_UK";
     catalogName = "wp";
 
-    String vAppTemplateName = "satya_demo_template";
-    String vAppName = "satya_test_vapp";
-    String vmName = "satyarhel";
-    Integer noOfBlankVms = 1;
-
     System.out.println("Blank VM's Sample");
     System.out.println("-----------------");
-
-    vcloudClient = new VcloudClient(vCloudUrl, Version.V5_1);
-    // change log levels if needed.
-    VcloudClient.setLogLevel(Level.ALL);
-    vcloudClient.registerScheme("https", 443,
-        FakeSSLSocketFactory.getInstance());
-    System.out.println("---------" + username + password);
-    vcloudClient.login(username, password);
-
-    // find org
-    Organization org = Organization.getOrganizationByReference(vcloudClient,
-        vcloudClient.getOrgRefByName(orgName));
-    System.out.println("Organization :: " + org.getReference().getName());
-
-    // find vdc
-    vdc = Vdc.getVdcByReference(vcloudClient, org.getVdcRefByName(vdcName));
-    System.out.println("Vdc :: " + vdc.getReference().getName());
+    
+    ReferenceType catalogRef = setupVCloudParams(vCloudUrl, username, password,
+        orgName, vdcName, catalogName);
 
     // upload vapptemplate
-
-    vappTemplate = uploadVappTemplate(vAppTemplateName, vAppTemplateName,
-        new ByteArrayInputStream(null), null);
-    System.out.println("Created VappTemplate :: " + vAppTemplateName);
-
-    // add to catalog
-    ReferenceType catalogRef = null;
-    for (ReferenceType reference : org.getCatalogRefs()) {
-      if (reference.getName().equals(catalogName)) {
-        catalogRef = reference;
-        break;
+    while (StdIn.hasNextLine()) {
+      String name = StdIn.readString();
+      String ovfLoc = StdIn.readString();
+      String vmdkLoc = StdIn.readString();
+      String desc = StdIn.readString();
+      InputStream ovfStream = null, vmdkStream = null;
+      
+      try {
+        ovfStream = new FileInputStream(new File(ovfLoc));
+        vmdkStream = new FileInputStream(new File(vmdkLoc));
+        VappTemplate vtmpl = uploadVappTemplate(name, desc, vmdkLoc, ovfStream, vmdkStream);
+        addVappTemplateToCatalog(catalogRef, vtmpl, name, desc);        
+      } finally {
+        if (ovfStream!=null)
+          ovfStream.close();
+        if (vmdkStream!=null)
+          vmdkStream.close();
       }
+      
     }
-
-    addVappTemplateToCatalog(catalogRef, vAppTemplateName);
-    System.out.println("Added VappTemplate to Catalog :: "
-        + catalogRef.getName());
 
     /****
      * VERIFICATION // creating vapp with the specified no of blank vm's.
+     *
+     * String vAppTemplateName = "satya_demo_template";
+     * String vAppName = "satya_test_vapp";
+     * String vmName = "satyarhel";
+     * Integer noOfBlankVms = 1;
      * createVapp(vAppName, vmName, noOfBlankVms);
+     *
      * System.out.println("Created Vapp :: " + vapp.getReference().getName());
-     * System.out.println("		VM Names"); System.out.println("		--------"); for
-     * (VM vm : vapp.getChildrenVms()) { System.out.println("		" +
-     * vm.getResource().getName()); }
+     * System.out.println("		VM Names"); 
+     * System.out.println("		--------"); 
+     * for(VM vm : vapp.getChildrenVms()) { 
+     * System.out.println("		" + vm.getResource().getName()); }
      */
+  }
+
+  private static ReferenceType setupVCloudParams(String vCloudUrl,
+      String username, String password, String orgName, String vdcName,
+      String catalogName) throws KeyManagementException,
+      UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {
+    ReferenceType catalogRef = null;
+
+    try {
+
+      vcloudClient = new VcloudClient(vCloudUrl, Version.V5_1);
+      // change log levels if needed.
+      VcloudClient.setLogLevel(Level.ALL);
+      vcloudClient.registerScheme("https", 443, FakeSSLSocketFactory.getInstance());
+      //System.out.println("---------" + username + password);
+      vcloudClient.login(username, password);
+
+      // find org
+      Organization org = Organization.getOrganizationByReference(vcloudClient,
+          vcloudClient.getOrgRefByName(orgName));
+      System.out.println("Organization :: " + org.getReference().getName());
+  
+      // find vdc
+      vdc = Vdc.getVdcByReference(vcloudClient, org.getVdcRefByName(vdcName));
+      System.out.println("Vdc :: " + vdc.getReference().getName());
+  
+      // add to catalog
+      for (ReferenceType reference : org.getCatalogRefs()) {
+        if (reference.getName().equals(catalogName)) {
+          catalogRef = reference;
+          break;
+        }
+      }
+      
+    } catch (VCloudException vcex) {
+      System.err.println("Error communicating:" + vcex.getMessage());
+      System.exit(1);
+    }
+    return catalogRef;
   }
 }
