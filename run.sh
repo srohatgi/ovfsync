877c6c1064b9ca37ab058b33aa72a853da967942
Sumeet Rohatgi <sumeet_rohatgi@hotmail.com>
2013-10-04 12:00:30 -0500

    made it more full fledged

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -cp bin:libs/VMware-vCloudDirector-JavaSDK/SDK-5.1.0/vcloud-java-sdk-5.1.0.jar catalog.Uploader
+cp=$(find libs -name '*.jar' -print | awk 'BEGIN { cp=""; } { cp = sprintf("%s:%s",cp,$0); } END { print cp; }')
+java -cp bin:$cp catalog.Uploader $@
